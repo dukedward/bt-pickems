45726aedde087505557fe97bbbfa3f9c7d0aed02
Dukeward <dukeward@Edwards-MacBook-Pro-2.local>
2026-04-25 12:59:03 -0400

    Added PWA and icons

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "node:path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "",
+            sizes: "",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
-})
+    },
+  },
+});
