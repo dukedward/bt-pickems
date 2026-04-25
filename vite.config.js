fa08a3e732918c3f900672508c689a2785647490
Dukeward <dukeward@Edwards-MacBook-Pro-2.local>
2026-04-25 13:35:01 -0400

    Remove PWA

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        icons: [
-          {
-            src: "",
-            sizes: "",
-            type: "image/png",
-            purpose: "any",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
